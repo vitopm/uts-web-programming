331049358cd8075c70fc93cdeff9e5d42968f817
Vito <vito.pramudita1502@gmail.com>
2022-11-26 00:24:21 +0700

    Added route for contact

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use Illuminate\Routing\RouteAction;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,6 +29,8 @@ Route::get('/publisher', function () {
     return view('publisher');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+Route::get('/contact', [HomeController::class, 'contact']);
