28c24e076d6be4a6f0b84dd7da04f7214021f522
Vito <vito.pramudita1502@gmail.com>
2022-11-26 00:17:43 +0700

    Added new route to get to home page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
-    return view('home');
-});
+// Route::get('/home', function () {
+//     return view('home');
+// });
 
+Route::get('/home', [HomeController::class, 'index']);
 
 // Route::get('/', function () {
 //     return view('home');
