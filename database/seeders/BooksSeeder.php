c32d29fa142f09f639c658320a0593e8e1af62d5
Vito <vito.pramudita1502@gmail.com>
2022-11-26 17:24:51 +0700

    Added book seeder

---
+++ database/seeders/BooksSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+
+class BooksSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+        for($i=0; $i<=9; $i++){
+// id (PK)	publisher_id (FK)	title	author	year	sysnopsis	image
+// publisher_id 	title 	author 	year 	synopsis 	image
+            DB::table('books')->insert([
+                'publisher_id'=>mt_rand(1,10),
+                'title'=>$faker->title(),
+                'author' =>$faker->name(),
+                'year'=>$faker->year(),
+                'synopsis'=>$faker->paragraph(),
+                'image'=>$faker->imageUrl()
+            ]);
+        }
+
+    }
+}
--- database/seeders/PublishersSeeder.php
+++ database/seeders/PublishersSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class PublishersSeeder extends Seeder
 {
@@ -14,5 +16,15 @@ class PublishersSeeder extends Seeder
     public function run()
     {
         //
+        $faker = Faker::create();
+        for($i=0; $i<=9; $i++){
+            DB::table('publishers')->insert([
+                'name'=>$faker->company(),
+                'address'=>$faker->address(),
+                'phone' =>"+628".$faker->numberBetween(111111111,999999999),
+                'email'=>$faker->unique()->safeEmail(),
+                'image'=>$faker->imageUrl()
+            ]);
+        }
     }
 }
