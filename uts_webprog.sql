c8e6ccdb440db03f0e310f1bc205851ffb867204
Vito <vito.pramudita1502@gmail.com>
2022-11-28 14:00:44 +0700

    Export sql database

--- uts_webprog.sql
+++ uts_webprog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2022 at 07:38 AM
+-- Generation Time: Nov 28, 2022 at 08:00 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.7
 
