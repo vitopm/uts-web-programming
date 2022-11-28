3fa6396e2a8fa37779211ccc12864d017475a00a
Vito <vito.pramudita1502@gmail.com>
2022-11-28 10:15:02 +0700

    Added data array for seeding

--- database/seeders/BooksSeeder.php
+++ database/seeders/BooksSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
 class BooksSeeder extends Seeder
@@ -15,17 +16,47 @@ class BooksSeeder extends Seeder
      */
     public function run()
     {
+        $title=array(
+            'The Tools',
+            'Coming Alive',
+            'Atomic Habits',
+            'Naruto and Saburo',
+            'Twilight',
+            'Blue Lock',
+            'Sakamoto Days',
+            'Pokemon Adventures: XY',
+            'IPA Terpadu SMP Kelas 9 K13 Revisi 2016',
+            'Buku Matematika Peminatan Kelas 12 Kurikulum 2013',
+
+
+
+        );
+        $image_url=array(
+            'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/8129/9780812983043.jpg',
+            'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/8129/9780812984545.jpg',
+            'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/7352/9780735211292.jpg',
+            'https://upload.wikimedia.org/wikipedia/en/9/94/NarutoCoverTankobon1.jpg',
+            'http://3.bp.blogspot.com/-5oAChzeMCzM/Ts5EU9wvx1I/AAAAAAAAAx0/V-RUDtHRYOI/s1600/TwilightGraphicNovelV2_HC.jpg',
+            'https://imgix.ranker.com/user_node_img/50130/1002585104/original/1002585104-photo-u-227601860?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=650',
+            'https://imgix.ranker.com/user_node_img/50130/1002584839/original/1002584839-photo-u1797710413?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=650',
+            'https://imgix.ranker.com/user_node_img/50130/1002585008/original/1002585008-photo-u-705150765?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=650',
+            'http://yudhistira-gi.co.id/assets/uploads/product/A-IPA-Terpadu-9-K13.jpg',
+            'https://s4.bukalapak.com/img/4082129582/w-1000/BUKU_PR_MATEMATIKA_WAJIB_SMA_KELAS_12_1_TAHUN_INTAN_PARIWARA.jpg'
+        );
+
         $faker = Faker::create('id_ID');
         for($i=0; $i<=9; $i++){
 // id (PK)	publisher_id (FK)	title	author	year	sysnopsis	image
 // publisher_id 	title 	author 	year 	synopsis 	image
             DB::table('books')->insert([
                 'publisher_id'=>mt_rand(1,10),
-                'title'=>$faker->sentence(1,3),
+                // 'title'=>$faker->sentence(1,3),
+                'title'=>$title[$i],
                 'author' =>$faker->name(),
                 'year'=>$faker->year(),
                 'synopsis'=>$faker->paragraph(),
-                'image'=>$faker->imageUrl()
+                // 'image'=>$faker->imageUrl()
+                'image'=>$image_url[$i]
             ]);
         }
     }
--- database/seeders/PublishersSeeder.php
+++ database/seeders/PublishersSeeder.php
@@ -16,6 +16,19 @@ class PublishersSeeder extends Seeder
     public function run()
     {
         //
+        $image_url = array(
+            'http://www.bardfieldacademy.org/wp-content/uploads/2016/08/00106-3D-company-logos-design-free-logo-online-02.png',
+            'http://lofrev.net/wp-content/photos/2014/10/H-logo.jpg',
+            'http://www.datwebdigital.com/DWD/wp-content/uploads/2012/06/logo-design.jpg',
+            'https://cdn.windowsreport.com/wp-content/uploads/2017/09/logo-design-software.png',
+            'https://www.logolynx.com/images/logolynx/20/2043aa2034232d6b02b3ecd8cad8a287.jpeg',
+            'http://2.bp.blogspot.com/-ZwQGP-U8TP4/UHsoCz3362I/AAAAAAAAA5w/2ckbMSSQY_E/s1600/Logo+2.jpg',
+            'http://lofrev.net/wp-content/photos/2016/07/abstract_logo_vector.png',
+            'https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/KFC_logo.svg/1200px-KFC_logo.svg.png',
+            'http://www.alancoleman.co.uk/wp-content/uploads/2014/10/cardinals_logo.png',
+            'https://png.pngtree.com/png-vector/20200921/ourlarge/pngtree-red-and-black-logo-png-image_2348180.jpg'
+
+        );
         $faker = Faker::create('id_ID');
         for($i=0; $i<=9; $i++){
             DB::table('publishers')->insert([
@@ -23,7 +36,7 @@ class PublishersSeeder extends Seeder
                 'address'=>$faker->address(),
                 'phone' =>"+628".$faker->numberBetween(111111111,999999999),
                 'email'=>$faker->unique()->safeEmail(),
-                'image'=>$faker->imageUrl()
+                'image'=>$image_url[$i]
             ]);
         }
     }
